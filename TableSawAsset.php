26580c694a931e1f324b9bb9bc8f1626acc26408
Dave <dave@virtual-wonders.com>
2018-06-13 16:19:15 +0800

    Removed src folder

--- TableSawAsset.php
+++ TableSawAsset.php
@@ -11,7 +11,7 @@ use yii\web\AssetBundle;
  */
 class TableSawAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/virtualwonders/yii2-tablesaw/src/dist';
+    public $sourcePath = '@vendor/virtualwonders/yii2-tablesaw/dist';
     public $css = [
         'tablesaw.css',
     ];
